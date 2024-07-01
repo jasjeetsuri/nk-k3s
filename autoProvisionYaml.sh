13c9434c10dfcc2c9d506b01041a7d4d3f21bd55
Jas Suri <33685132+jasjeetsuri@users.noreply.github.com>
2024-07-01 20:58:46 +0100

    Update autoProvisionYaml.sh

--- autoProvisionYaml.sh
+++ autoProvisionYaml.sh
@@ -16,10 +16,12 @@ uninstall_k3s() {
 }
 
 install_dependancies() {
-  echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+
   apt update
-  apt install curl
+  apt install curl sudo -y
+  echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
   curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+  apt update
   apt install libedgetpu1-std gnupg  wget sudo jq nfs-common git open-iscsi intel-media-va-driver -y
 }
 
